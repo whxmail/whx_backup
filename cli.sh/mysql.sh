6c643641fe8571e53e11edbd679d80feadca418b
whxmail <syseye@163.com>
2016-05-14 16:02:23 +0800

    changed

--- cli.sh/mysql.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker run --name whx-mysql -e MYSQL_ROOT_PASSWORD=191908577 -d whx/mysql
-
--- cli.sh/nginx.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker run --name whx-nginx --link whx-mysql:mysql -d -P -p 80:80 -v $PWD/whxmail.com:/var/www/whxmail.com whx/nginx
-
